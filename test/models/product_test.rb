71b947fb844f1a7876794e465796bf563dfc9585
Gusepe Werdy <gusepe.werdy@gmail.com>
2014-03-15 23:44:22 +0100

    Create Cart

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -68,7 +68,7 @@ class ProductTest < ActiveSupport::TestCase
 
 	test "product is not valid without a unique title - i18n" do
 		product = Product.new(
-			title:products(:ruby).title,
+			title: products(:ruby).title,
 			description: "yyy",
 			price: 1,
 			image_url: "fred.gif")
