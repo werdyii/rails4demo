1814672e41cf9cb00d607997c2e7e611687cee0c
Gusepe Werdy <gusepe.werdy@gmail.com>
2014-03-16 01:11:43 +0100

    Add price to cart, Deleting items cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315224645) do
+ActiveRecord::Schema.define(version: 20140315230235) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
